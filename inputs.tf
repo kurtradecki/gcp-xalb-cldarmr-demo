4e100847f775269ff0ec2f818023832e54e8bede
kurtradecki <156200507+kurtradecki@users.noreply.github.com>
2025-06-29 17:39:45 -0400

    Create inputs.tf

---
+++ inputs.tf
@@ -0,0 +1,75 @@
+variable project-name-and-id {
+  type = string
+}
+
+variable gcp-region {
+  type = string    
+}
+
+variable gcp-zone {
+  type = string    
+}
+
+variable vpc-name {
+  type = string    
+}
+
+variable url-map-name {
+  type = string    
+}
+
+variable vm-name {
+  type = string    
+}
+
+variable instance-group-1-name-prefix {
+    type = string
+}
+
+variable cert-name-prefix {
+  type = string
+}
+
+variable lb-static-ip-name-prefix {
+  type = string    
+}
+
+variable cloudarmor-policy-name-prefix{
+  type = string
+}
+
+variable health-check-name-prefix {
+  type = string
+}
+
+variable forwarding-rule-name-prefix {
+  type = string    
+}
+
+variable proxy-http-name-prefix {
+  type = string   
+}
+
+variable backend-service-name-prefix {
+  type = string
+}
+
+variable fwr-health-check-name-prefix {
+  type = string
+}
+
+variable named-port-name {
+  type = string
+}
+
+variable named-port {
+  type = string
+}
+
+variable backend-protocol {
+  type = string
+}
+
+variable name-base {
+  type = string
+}
