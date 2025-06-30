c6ef3d1635294773e48c9247a7bc725ea5dcaca2
kurtradecki <156200507+kurtradecki@users.noreply.github.com>
2025-06-29 22:11:41 -0400

    feat: net-infra-and-cleanup (#1)
    Co-authored-by: Kurt Radecki <radecki@google.com>

--- main.tf
+++ main.tf
@@ -25,44 +25,102 @@ terraform {
   }
 }
 
+/*
 data "google_compute_instance" "vm" {
-  project = var.project-name-and-id
-  name = var.vm-name
-  zone = var.gcp-zone
+  project = var.project_id
+  name    = var.vm_name
+  zone    = "${var.gcp_region}${var.gcp_zone_suffix}"
+}
+*/
+
+# create VPC and subnets
+module "vpc" {
+  source     = "github.com/terraform-google-modules/cloud-foundation-fabric//modules/net-vpc?ref=v36.0.0"
+  project_id = var.project_id
+  name       = var.vpc_name
+  subnets = [merge(var.vpc_subnets, {
+    region = var.gcp_region
+  })]
+}
+
+# Public NAT for web server to download nginx
+module "cldnat-websrvr" {
+  source         = "github.com/terraform-google-modules/cloud-foundation-fabric//modules/net-cloudnat?ref=v36.0.0"
+  project_id     = var.project_id
+  region         = var.gcp_region
+  name           = "${var.cldnat_name}-${var.vpc_name}"
+  router_network = var.vpc_name
+}
+
+# create web server VM
+resource "google_compute_instance" "websrvr_vm" {
+  boot_disk {
+    auto_delete = true
+    device_name = "websrvr"
+
+    initialize_params {
+      image = "projects/debian-cloud/global/images/debian-12-bookworm-v20250610"
+      size  = 10
+      type  = "pd-balanced"
+    }
+
+    mode = "READ_WRITE"
+  }
+  machine_type = "e2-micro"
+  name = var.vm_name
+  metadata = {
+    enable-osconfig = "TRUE"
+    enable-oslogin  = "true"
+    startup-script  = "sudo apt-get update\nsudo apt-get install -y nginx\nsudo systemctl start nginx"
+  }
+  network_interface {
+  #  network = "https://www.googleapis.com/compute/v1/projects/${var.project_id}/global/networks/${var.vpc_name}"
+    stack_type         = "IPV4_ONLY"
+    subnetwork         = "https://www.googleapis.com/compute/v1/projects/${var.project_id}/regions/${var.gcp_region}/subnetworks/${var.vpc_subnets.name}"
+  #  subnetwork_project = var.project_id
+  }
+  shielded_instance_config {
+    enable_integrity_monitoring = true
+    enable_secure_boot          = true
+    enable_vtpm                 = true
+  }
+  project = var.project_id
+  zone = "${var.gcp_region}${var.gcp_zone_suffix}"
+  depends_on = [ module.vpc, module.cldnat-websrvr ]
 }
 
 # create Instance group for the VM
 resource "google_compute_instance_group" "instance-group" {
-  project = var.project-name-and-id
-  name        = "${var.instance-group-1-name-prefix}-${var.url-map-name}"
+  project = var.project_id
+  name    = "${var.instance_group_1_name_prefix}-${var.url_map_name}"
   instances = [
-    data.google_compute_instance.vm.id,
+    google_compute_instance.websrvr_vm.id,
   ]
   named_port {
-    name = var.named-port-name
-    port = var.named-port
+    name = var.named_port_name
+    port = var.named_port
   }
-  zone = var.gcp-zone
+  zone = "${var.gcp_region}${var.gcp_zone_suffix}"
 }
 
 # create global static external IP address used to reach the load balancer
 resource "google_compute_global_address" "lb-static-ip" {
-  project = var.project-name-and-id
-  name         = "${var.lb-static-ip-name-prefix}-${var.url-map-name}"
+  project      = var.project_id
+  name         = "${var.lb_static_ip_name_prefix}-${var.url_map_name}"
   address_type = "EXTERNAL"
   ip_version   = "IPV4"
 }
 
 # enable certificatemanager api
 resource "google_project_service" "certificatemanager-api" {
-  project = var.project-name-and-id
+  project = var.project_id
   service = "certificatemanager.googleapis.com"
 }
 
 # create Cert ca-demo-cert
 resource "google_compute_managed_ssl_certificate" "cert" {
-  project = var.project-name-and-id
-  name = var.cert-name-prefix
+  project = var.project_id
+  name    = var.cert_name_prefix
   managed {
     domains = ["${google_compute_global_address.lb-static-ip.address}.nip.io"]
   }
@@ -70,8 +128,8 @@ resource "google_compute_managed_ssl_certificate" "cert" {
 
 # create Cloud Armor policy for only allowed IPs
 resource "google_compute_security_policy" "cloudarmor-policy" {
-  project = var.project-name-and-id
-  name = "${var.cloudarmor-policy-name-prefix}-${var.url-map-name}"
+  project = var.project_id
+  name    = "${var.cloudarmor_policy_name_prefix}-${var.url_map_name}"
   rule {
     action   = "allow"
     priority = "10000"
@@ -100,10 +158,10 @@ resource "google_compute_security_policy" "cloudarmor-policy" {
 
 # health check
 resource "google_compute_health_check" "health-check" {
-  project = var.project-name-and-id
-  name     = "${var.health-check-name-prefix}-${var.url-map-name}"
+  project = var.project_id
+  name    = "${var.health_check_name_prefix}-${var.url_map_name}"
   tcp_health_check {
-    port = var.named-port
+    port = var.named_port
   }
 }
 
@@ -111,15 +169,15 @@ resource "google_compute_health_check" "health-check" {
 
 # backend service with custom request and response headers
 resource "google_compute_backend_service" "backend-service" {
-  project = var.project-name-and-id
-  name                    = "${var.backend-service-name-prefix}-${var.url-map-name}"
-  protocol                = var.backend-protocol
-  port_name               = var.named-port-name
-  load_balancing_scheme   = "EXTERNAL"
-  timeout_sec             = 10
-  enable_cdn              = false
-#  security_policy         = google_compute_security_policy.cloudarmor-policy.id
-  health_checks           = [google_compute_health_check.health-check.id]
+  project               = var.project_id
+  name                  = "${var.backend_service_name_prefix}-${var.url_map_name}"
+  protocol              = var.backend_protocol
+  port_name             = var.named_port_name
+  load_balancing_scheme = "EXTERNAL"
+  timeout_sec           = 10
+  enable_cdn            = false
+  #  security_policy         = google_compute_security_policy.cloudarmor-policy.id
+  health_checks = [google_compute_health_check.health-check.id]
   backend {
     group           = google_compute_instance_group.instance-group.id
     balancing_mode  = "UTILIZATION"
@@ -129,11 +187,11 @@ resource "google_compute_backend_service" "backend-service" {
 
 # create Firewall rules for load balancer health check
 resource "google_compute_firewall" "fwr-health-check" {
-  project = var.project-name-and-id
-  name    = var.fwr-health-check-name-prefix
+  project   = var.project_id
+  name      = var.fwr_health_check_name_prefix
   direction = "INGRESS"
-  priority = "1000"
-  network = var.vpc-name
+  priority  = "1000"
+  network   = var.vpc_name
   allow {
     protocol = "tcp"
   }
@@ -145,23 +203,23 @@ resource "google_compute_firewall" "fwr-health-check" {
 
 # url map
 resource "google_compute_url_map" "url-map" {
-  project = var.project-name-and-id
-  name            = "${var.url-map-name}-${var.url-map-name}"
+  project         = var.project_id
+  name            = "${var.url_map_name}-${var.url_map_name}"
   default_service = google_compute_backend_service.backend-service.id
 }
 
 # https proxy
 resource "google_compute_target_https_proxy" "proxy-https" {
-  project = var.project-name-and-id
-  name     = "${var.proxy-http-name-prefix}s-${var.url-map-name}"
-  url_map  = google_compute_url_map.url-map.id
+  project          = var.project_id
+  name             = "${var.proxy_http_name_prefix}s-${var.url_map_name}"
+  url_map          = google_compute_url_map.url-map.id
   ssl_certificates = [google_compute_managed_ssl_certificate.cert.id]
 }
 
 # forwarding rule for https
 resource "google_compute_global_forwarding_rule" "forwarding-rule-https" {
-  project = var.project-name-and-id
-  name                  = "${var.forwarding-rule-name-prefix}-https-${var.url-map-name}"
+  project               = var.project_id
+  name                  = "${var.forwarding_rule_name_prefix}-https-${var.url_map_name}"
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL"
   port_range            = "443"
@@ -171,15 +229,15 @@ resource "google_compute_global_forwarding_rule" "forwarding-rule-https" {
 
 # http proxy
 resource "google_compute_target_http_proxy" "proxy-http" {
-  project = var.project-name-and-id
-  name     = "${var.proxy-http-name-prefix}-${var.url-map-name}"
-  url_map  = google_compute_url_map.url-map.id
+  project = var.project_id
+  name    = "${var.proxy_http_name_prefix}-${var.url_map_name}"
+  url_map = google_compute_url_map.url-map.id
 }
 
 # forwarding rule for http
 resource "google_compute_global_forwarding_rule" "forwarding-rule-http" {
-  project = var.project-name-and-id
-  name                  = "${var.forwarding-rule-name-prefix}-http-${var.url-map-name}"
+  project               = var.project_id
+  name                  = "${var.forwarding_rule_name_prefix}-http-${var.url_map_name}"
   ip_protocol           = "TCP"
   load_balancing_scheme = "EXTERNAL"
   port_range            = "80"
